7e6229d2cf3c982c64a51d245a9596f489fca380
Dean <dean.oliver@codedigm.co.za>
2020-03-29 15:16:50 +0200

    added shared module and fixed routing

--- projects/za-realty-app/src/app/core/core.module.ts
+++ projects/za-realty-app/src/app/core/core.module.ts
@@ -1,23 +1,24 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
-import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule} from '@angular/material/button';
+
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [MainLayoutComponent],
   imports: [
-    //vendor
+    // vendor
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule,
 
-    //material
+    // material
     MatToolbarModule,
     MatButtonModule
   ],
   exports: [MainLayoutComponent]
 })
-export class CoreModule { }
+export class CoreModule {}
