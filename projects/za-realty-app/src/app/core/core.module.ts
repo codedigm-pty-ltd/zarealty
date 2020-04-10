73e67f73ebabf4f1a9ab586b8cd2c776f4219342
Dean <dean.oliver@codedigm.co.za>
2020-04-10 13:44:47 +0200

    added house key style sheets

--- projects/za-realty-app/src/app/core/core.module.ts
+++ projects/za-realty-app/src/app/core/core.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
@@ -19,7 +20,8 @@ import { MainLayoutComponent } from './layout/main-layout/main-layout.component'
     // material
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatProgressSpinnerModule
   ],
   exports: [MainLayoutComponent]
 })
